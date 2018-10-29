f88bce5d042ee210c1f80d5579843fc4d3aa4879
Steven <stevenawillets@gmail.com>
2018-10-29 00:10:29 -0400

    updated css and js

--- scripts/portfolio-main.js
+++ scripts/portfolio-main.js
@@ -1,10 +1,38 @@
+function checkArrows(target){
+	if (target.next().length == 0) {
+		$('#next-slide').hide()
+	}
+	if (target.prev().length == 0) {
+		$('#prev-slide').hide()
+	}
+}
+
 $('.inactive').click(function(){
-	$(this).removeClass('inactive').addClass('active')
-	$('.page-container').addClass('modal-open')
+	var $slide = $(this).find('.tile-content')
+	$(this).addClass('active').removeClass('inactive');
+	$('body').addClass('modal-open');
+	$('.move-slide').show();
+	$('#controls').css({'height': $slide.height(), 'width': $slide.width()})
+	checkArrows($('.active'))
 });
-$('.close, #overlay').click(function(e){
-	e.stopPropagation()
-	e.preventDefault()
-	$('.active').removeClass('active').addClass('inactive')
-	$('.page-container').removeClass('modal-open')
+
+$('.move-slide').click(function(e){
+	e.stopPropagation();
+	e.preventDefault();
+	$('.active').addClass('current')
+	$('.move-slide').show();
+	var $current = $('.current'),
+		$next = $(this).attr('href') == 'next' ? $current.next() : $current.prev();
+	checkArrows($next)
+	$current.addClass('inactive').removeClass('active current');
+	$next.addClass('active').removeClass('inactive')
+
 });
+
+$('#close-slide, #overlay').click(function(e){
+	e.stopPropagation();
+	e.preventDefault();
+	$('.active').removeClass('active').addClass('inactive');
+	$('body').removeClass('modal-open');
+});
+
